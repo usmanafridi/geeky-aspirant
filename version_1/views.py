e3c47293f991dcf0694857816adc6a067afae24a
Apple <apple@Apples-iMac.local>
2022-11-18 15:56:08 +0500

    second update

--- version_1/views.py
+++ version_1/views.py
@@ -16,25 +16,46 @@ openai.api_key = "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"
 
 
 def index(request):
+    """This function is written so that I can test how to write post text and get it to the backend of our system"""
     
     if request.method == 'POST':
-        
         username = request.POST['username']
-        print(f"The name {username} has been entered")
+        print(username)
+        return HttpResponse(f"Hello, {username}. Pleased to meet you.")   
         
     return render(request, 'index.html')
 
 
 
-def outline(request):
-    response = openai.Completion.create(
-    model="text-davinci-002",
-    prompt="Create outline for topic: Time and tide waits for none\n\n",
-    temperature=0.3,
-    max_tokens=331,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
 
-    return HttpResponse(response["choices"][0]["text"])
+
+def outline(request):
+    """This function is to create outline of a text provided by the user"""
+    
+    if request.method == 'POST':
+
+        text = request.POST['text']  #The text we will write
+        
+        response = openai.Completion.create(
+        model="text-davinci-002",
+        
+        prompt=text,
+        temperature=0.3,
+        max_tokens=331,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+        
+        text_response = response["choices"][0]["text"]
+        
+        context = {
+           
+        "htmltext": text_response
+    }
+       
+        return render(request, 'outline_results.html', context)
+
+
+        
+    return render(request, 'outline.html')
