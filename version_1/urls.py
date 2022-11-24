d6bb27c366a1a9d3c59c9bb2a723f6561f0b2a9c
Apple <apple@Apples-iMac.local>
2022-11-24 09:43:25 +0500

    updated more

--- version_1/urls.py
+++ version_1/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 urlpatterns = [
     path("text-outline", views.outline),
-    path("check", views.index),
+    # path("check", views.index),
     path("grammar", views.grammar_correction),
     path("summary", views.text_summarizer),
+    path("sentence", views.word_mean_sentence),
+    path("", views.index),
+    path("syn_ant", views.syn_anto),
    
 
 ]
--- version_1/views.py
+++ version_1/views.py
@@ -6,20 +6,28 @@ import openai
 openai.api_key = "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"
 
 
+def gpt3(prompt):
+
+    func = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=prompt,
+    temperature=0.3,
+    max_tokens=700,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+    )
+    result= func["choices"][0]["text"]
+    return result
+
 
 
 
 def index(request):
     """This function is written so that I can test how to write post text and get it to the backend of our system"""
     
-    if request.method == 'POST':
-        username = request.POST['username']
-        print(username)
-        return HttpResponse(f"Hello, {username}. Pleased to meet you.")   
         
-    return render(request, 'index.html')
-
-
+    return render(request, 'textbox.html')
 
 
 
@@ -29,19 +37,8 @@ def outline(request):
     if request.method == 'POST':
 
         text = request.POST['text']  #The text we will write
-        
-        response = openai.Completion.create(
-        model="text-davinci-002",
-        
-        prompt=text,
-        temperature=0.3,
-        max_tokens=331,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
-        
-        text_response = response["choices"][0]["text"]
+
+        text_response = gpt3(text)
 
         #The context will return the text response
 
@@ -57,26 +54,15 @@ def outline(request):
 
 
 
-
 def grammar_correction(request):
     """This function is to correct any grammatical mistake in a sentence"""
     
     if request.method == 'POST':
         
         text = request.POST['text']  #The text we will write
-        grammar_sentence = openai.Completion.create(
-        model="text-davinci-002",
-        
-        prompt= f"Correct this to standard English:\n\n + {text}",
-        temperature=0,
-        max_tokens=60,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-        )
-
+       
         #The context will return the grammatically correct sentence
-        correct_sentence= grammar_sentence["choices"][0]["text"]
+        correct_sentence= gpt3(f"Correct this to standard English:\n\n + {text}")
         #By specifying the name of the context in the html, it will display the results.
         
         context = {           
@@ -90,10 +76,8 @@ def grammar_correction(request):
     return render(request, 'grammar.html')
 
 
-
-
-
 def text_summarizer(request):
+
     """This function is to make a short summary of a text"""
     
     if request.method == 'POST':
@@ -144,3 +128,77 @@ def text_summarizer(request):
 
         
     return render(request, 'summary.html')
+
+
+def word_mean_sentence(request):
+
+    """This function is to make sentences and write the meaning of the words"""
+    
+    if request.method == 'POST':
+        
+        text = request.POST['text']  #The text we will write
+        ## Here, the title will be generated  
+        
+        #return title
+        word_meaning= gpt3(f"Write the meaning of {text} in 1. English and 2. Urdu.\n\n")
+        
+        #return sentence
+        word_sentence= gpt3(f"Use {text} in a sentence",)
+        
+        #By specifying the name of the context in the html, it will display the results.
+        context = {           
+        "word_meaning": word_meaning,
+        "word_sentence": word_sentence,
+    }       
+        return render(request, 'sentences.html', context)
+  
+    return render(request, 'sentences.html')
+
+
+
+def syn_anto(request):
+
+    """This function is to generate synonyms and antonyms"""
+    
+    if request.method == 'POST':
+        
+        text = request.POST['text']  #The text we will write
+ 
+        #return meaning
+        word_meaning= gpt3(f"Write the meaning of {text} in 1. English and 2. Urdu.\n\n")
+        
+        #return syn
+        word_synonym= gpt3(f"Write 3 synonyms of {text}")
+        
+        #return ant
+        word_antonym= gpt3(f"Write 3 antonyms of {text}")
+        
+        #By specifying the name of the context in the html, it will display the results.
+        
+        context = {           
+        "word_synonym": word_synonym,
+        "word_antonym": word_antonym,
+        "word_meaning":word_meaning
+
+    }
+       
+        return render(request, 'synonym.html', context)
+
+
+        
+    return render(request, 'synonym.html')
+
+
+
+
+
+## Synonyms
+## Antonyms
+
+## Rewrite the sentence without changing the meaning.
+
+## Fill the word with grammar/ appropriate word.
+
+## Use word in a sentence.
+
+## Use if idioms in a sentence
